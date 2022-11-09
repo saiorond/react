d367de2e6b9d32f68551a16610f7d331fc1d4b12
Débora <d.saioron@hotmail.com>
2022-11-09 20:44:33 -0300

    estilização

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import config from "../config.json";
+import styled from "styled-components";
 
 function HomePage() {
     const estiloHomePage = {backgroundColor: "red"};
@@ -15,21 +16,42 @@ function HomePage() {
 export default HomePage
 
 function Menu(){
-    return (
-        <div>
-            <img src="banner" />
-            <img src={`https://github.com/${config.github}.png`} />
-            {config.name}
-            {config.job}
-        </div>
-    )
+    <div>
+        Menu
+    </div>
 }
+
+const StyledHeader = styled.div `
+    img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+    .user-info {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
+    }
+`;
 
 function Header(){
     return (
-        <div>
-            Header
-        </div>
+        <StyledHeader>
+
+            <section className="user-info">
+                <img src={`https://github.com/${config.github}.png`} />
+                <div>
+                    <h2>
+                        {config.name}
+                        </h2>
+                    <p>
+                    {config.job}
+                    </p>
+                </div>
+            </section>
+        </StyledHeader>
     )
 }
 
